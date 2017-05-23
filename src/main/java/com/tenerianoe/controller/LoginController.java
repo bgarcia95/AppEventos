77a28ab21ce9027d3d88f500e3639d7ab62aca40
NERV <NERV@DESKTOP-HE0GL4J>
2017-05-23 14:52:24 -0600

    Mejoras Generales

--- src/main/java/com/tenerianoe/controller/LoginController.java
+++ src/main/java/com/tenerianoe/controller/LoginController.java
@@ -36,7 +36,7 @@ public class LoginController implements Serializable {
             us = usuarioEJB.iniciarSesion(usuario);
 
             if (us != null) {
-                redireccion = "/catalogos/proveedor/ProveedorAdm.xhtml";
+                redireccion = "/administracion/GestionUsuarioAdm.xhtml";
 
             } else {
                 FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "Aviso", "Error!"));
